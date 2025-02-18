722039e33180f24f6ebc9d0c71ac84226b800dbc
chw3021 <ckgusdnr1205@gmail.com>
2025-02-18 12:41:40 +0900

    250218

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,13 +1,9 @@
-
+import React from "react";
+import "../../styles/Button.css";
 
 
 const Button = ({text, type='default', onClick})=>{
-    const btnType = ["insertType", "cancelType"].includes(type) ? type : "default";
-
-    return(
-    <button className={["Button",`Button_${btnType}`].join(" ")}
-     onClick={onClick}>{text}</button>
-    );
+    return <button className="button">{text}</button>;
 };
 
 export default Button;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,19 @@
+import React from "react";
 import Button from "./Button";
+import "../../styles/Header.css";
+import logo from "../../assets/logo.png"; // 로고 이미지 경로
 
-const Header = () =>{
-
-    return(
+const Header = () => {
+    return (
         <div className="header">
-        <div>Musicovery</div>
-        <Button text={"홈"} />
-        <Button text={"소셜"} />
-        <Button text={"게시판"} />
+            <img src={logo} alt="Musicovery Logo" className="logo" />
+            <div className="nav">
+                <Button text={"홈"} />
+                <Button text={"소셜"} />
+                <Button text={"게시판"} />
+            </div>
         </div>
     );
-}
+};
 
 export default Header;
