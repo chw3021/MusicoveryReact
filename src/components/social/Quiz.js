bec293a081e53d428f384048ec1299765e3610ae
yejin040 <cndPwls04@gmail.com>
2025-02-21 12:02:34 +0900

    250221

--- src/components/social/Quiz.js
+++ src/components/social/Quiz.js
@@ -6,30 +6,57 @@ const Quiz = () =>{
 <div>
 <Header />
 
-    <div>
-        <div className="friendsListTool">
-            <div className="friendsListToolsection">
-                <section className="tool">친구</section>
-            <div className="friendsListToolsection2">
-                <div className="friendsListToolSelectsection">
-                <section className="friendsName">홍길동</section>
-                    <button type="button" className="friendsListsettingToolsection">정보</button>
-                    <button type="button" className="friendsListsettingToolsection">삭제</button>
+        <div className="quizView">
+            
+                <div className="friendsListTool">
+                    <div className="friendsListToolsection">
+                        <section className="tool">친구</section>
+                    <div className="friendsListToolsection2">
+                            <div className="friendsListToolSelectsection">
+                            <section className="friendsName">홍길동</section>
+                                <button type="button" className="friendsListsettingToolsection">정보</button>
+                                <button type="button" className="friendsListsettingToolsection">삭제</button>
+                            </div>
+                        <div className="friendsListToolSelectsection">
+                        <section className="friendsName">고양이</section>
+                            <button type="button" className="friendsListsettingToolsection">정보</button>
+                            <button type="button" className="friendsListsettingToolsection">삭제</button>
+                        </div>
+                        <div className="friendsListToolSelectsection">
+                        <section className="friendsName">강아지</section>
+                            <button type="button" className="friendsListsettingToolsection">정보</button>
+                            <button type="button" className="friendsListsettingToolsection">삭제</button>
+                        </div>
+                        <div className="friendsListToolSelectsection">
+                        <section className="friendsName">호랑이</section>
+                            <button type="button" className="friendsListsettingToolsection">정보</button>
+                            <button type="button" className="friendsListsettingToolsection">삭제</button>
+                        </div>
+                        <div className="friendsListToolSelectsection">
+                        <section className="friendsName">고라니</section>
+                            <button type="button" className="friendsListsettingToolsection">정보</button>
+                            <button type="button" className="friendsListsettingToolsection">삭제</button>
+                        </div>
+                        <div>
+                            <button type="button" className="next">next</button>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                <div className="friendsListToolSelectsection">
-                <section className="friendsName">고양이</section>
-                    <button type="button" className="friendsListsettingToolsection">정보</button>
-                    <button type="button" className="friendsListsettingToolsection">삭제</button>
+                <div className="playlistPygsong">
+                    <div className="playlistPygsongtool">
+                        <section className="playlistPygsongTitle">실행중인 플레이리스트</section>
+                        <div className="playlistPygsongView">
+                        </div>
+                    </div>
                 </div>
-                <div className="friendsListToolSelectsection">
-                <section className="friendsName">강아지</section>
-                    <button type="button" className="friendsListsettingToolsection">정보</button>
-                    <button type="button" className="friendsListsettingToolsection">삭제</button>
-                </div>
-            </div>
-           </div>
-        </div>
 
+            
+
+        
+        
+           
+        <div className="MainBackground"> dsa</div>
     </div>
 </div>
 
