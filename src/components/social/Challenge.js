497cd832dfb6e9da52d5350fa28204ad4f0f67f6
yejin040 <cndPwls04@gmail.com>
2025-02-21 10:37:49 +0900

    250221

--- src/components/social/Challenge.js
+++ src/components/social/Challenge.js
@@ -1,7 +1,12 @@
-
+import Header from "../common/Header";
 
 const Challenge = () =>{
-
+    return(
+        <div>
+            <Header />
+        </div>
+        
+            );
 }
 
 export default Challenge;
