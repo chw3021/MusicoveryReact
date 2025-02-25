e4608b7db8d9f27eb4ef85e0dda7042b9dc1290d
chw3021 <ckgusdnr1205@gmail.com>
2025-02-25 12:45:07 +0900

    250225

--- src/pages/PlaylistCreatePage.js
+++ src/pages/PlaylistCreatePage.js
@@ -4,6 +4,7 @@ import KeywordRecommendation from "../components/recommendations/KeywordRecommen
 import Header from "../components/common/Header";
 import Button from "../components/common/Button";
 import "../styles/playlistCreatePage.css";
+import AIRecommendations from "../components/recommendations/AIRecommendations";
 
 const PlaylistCreatePage = () => {
     const [activeComponent, setActiveComponent] = useState("create");
@@ -19,9 +20,11 @@ const PlaylistCreatePage = () => {
                 <div className="component_switcher">
                     <Button text="직접 만들기" onClick={() => handleComponentChange("create")} />
                     <Button text="자동 생성" onClick={() => handleComponentChange("keywordRecommendation")} />
+                    <Button text="AI 생성" onClick={() => handleComponentChange("aiRecommendation")} />
                 </div>
                 <div className="component_container">
                     {activeComponent === "keywordRecommendation" && <KeywordRecommendation />}
+                    {activeComponent === "aiRecommendation" && <AIRecommendations />}
                    <div className="component_style">
                     {activeComponent === "create" && <Create />}
                     </div>
