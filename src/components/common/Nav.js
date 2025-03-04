5b3864991dd1790395cb2d5fb3336f013778bea4
chw3021 <ckgusdnr1205@gmail.com>
2025-03-04 10:15:11 +0900

    250304

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -9,7 +9,6 @@ const Nav = () => {
                     <ul>
                         <li><Link to="/quiz">퀴즈</Link></li>
                         <li><Link to="/streaming">스트리밍</Link></li>
-                        <li><Link to="/challenge">첼린지</Link></li>
                     </ul>
                 </nav>
             </aside>
