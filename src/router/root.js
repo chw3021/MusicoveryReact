856d18e38cfc1a10ff258f21cf7b9b3fe83e3599
excel8057 <excel8057@naver.com>
2025-03-05 09:22:57 +0900

    250205

--- src/router/root.js
+++ src/router/root.js
@@ -28,6 +28,7 @@ const OAuthCallback = lazy(() => import("../components/auth/OAuthCallback"));
 const Streaming = lazy(() => import("../components/social/Streaming"));
 const Challenge = lazy(() => import("../components/social/Challenge"));
 const Quiz = lazy(() => import("../components/social/Quiz"));
+const SongQuiz = lazy(() => import("../components/social/SongQuiz"));
 const CustomerSupport = lazy(() =>
   import("../components/social/CustomerSupport")
 );
@@ -105,6 +106,16 @@ const root = createBrowserRouter(
           <Suspense fallback={Loading}>
             <PrivateRoute>
               <Quiz />
+            </PrivateRoute>
+          </Suspense>
+        }
+      />
+      <Route
+        path="songquiz"
+        element={
+          <Suspense fallback={Loading}>
+            <PrivateRoute>
+              <SongQuiz />
             </PrivateRoute>
           </Suspense>
         }
